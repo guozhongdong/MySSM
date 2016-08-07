d86aa83e74d750d106db8cf5d5eac6ebc06bdff7
guozhongdong <18272986912@163.com>
2016-08-07 17:16:01 +0800

    0807

---
+++ src/main/java/com/gzd/service/UserService.java
@@ -0,0 +1,10 @@
+package com.gzd.service;
+
+import java.util.List;
+
+import com.gzd.model.E_user;
+
+public interface UserService {
+
+	public List<E_user> loadQueryUser();
+}
---
+++ src/main/java/com/gzd/service/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.gzd.service.impl;
+
+import java.util.List;
+
+import com.gzd.dao.E_userMapper;
+import com.gzd.model.E_user;
+import com.gzd.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	private E_userMapper e_usermapper;
+	
+	public E_userMapper getE_usermapper() {
+		return e_usermapper;
+	}
+
+	public void setE_usermapper(E_userMapper e_usermapper) {
+		this.e_usermapper = e_usermapper;
+	}
+
+	@Override
+	public List<E_user> loadQueryUser() {
+		
+		
+		return e_usermapper.queryUser();
+	}
+
+	
+	public static void main(String[] args) {
+		
+		UserServiceImpl uServiceImpl = new UserServiceImpl();
+		List<E_user> list = uServiceImpl.loadQueryUser();
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.get(i));
+		}
+	}
+}
